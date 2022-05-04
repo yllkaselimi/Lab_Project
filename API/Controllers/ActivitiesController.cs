c3ac5c5c1ad08f14c8de83d14bc35debf3f8047c
Yllka Selimi <ys51425@ubt-uni.net>
2022-05-04 10:37:39 +0200

    Create Activity / CRUD

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -21,6 +21,13 @@ namespace API.Controllers
         public async Task<ActionResult<Activity>> GetActivity(Guid id)
         {
             return await Mediator.Send(new Details.Query{Id = id});    
-              }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateActivity(Activity activity)
+        {
+
+            return Ok(await Mediator.Send(new Create.Command{Activity = activity}));
+        }
     }
 }
---
+++ Application/Activities/Create.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Domain;
+using MediatR;
+using Persistance;
+
+namespace Application.Activities
+{
+    public class Create
+    {
+        public class Command : IRequest
+        {
+            public Activity Activity { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Command>
+        {
+        private readonly DataContext context;
+            public Handler(DataContext context)
+            {
+            this.context = context;
+            }
+
+            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            {
+                context.Activities.Add(request.Activity);
+
+                await context.SaveChangesAsync();
+
+                return Unit.Value;
+            }
+        }
+    }
+}
