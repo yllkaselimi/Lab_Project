26b865da94fb38c7613992d1b952be0a529b8514
Yllka Selimi <ys51425@ubt-uni.net>
2022-06-16 21:17:19 +0200

    Adding a NavBar

--- client-app/src/app/layouts/App.tsx
+++ client-app/src/app/layouts/App.tsx
@@ -2,37 +2,35 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Activity } from '../models/activity';
 import './styles.css';
+import NavBar from './NavBar';
 
-const baseURL = 'http://localhost:5000';
 function App() {
   const [activities, setActivities] = useState<Activity[]>([]);
 
   useEffect(() => {
-    axios.get(`${baseURL}/getAll`).then(response =>{
+    axios.get(`/getAll`).then(response =>{
       console.log('RESPONSE YLLKA: ', response);
       setActivities(response.data);
     })
   }, [])
 
-  useEffect(() => {
-    axios.get(`${baseURL}/5fcdba4c-b4da-4749-8d76-4d1037a4750a`).then(response =>{
-      console.log('Activity with id 2: ', response.data);
-    })
-  }, [])
-
+  
   return (
-    <div>
-      
-    <ul>
-      {activities.map((activity: any) => (
-        <li key={activity.id}>
-          {activity.title}
-        </li>
-     
-      ))}
-    </ul>
-    </div>
+    <>
+      <NavBar/>
+      <div id="body">
+        <ul>
+          {activities.map((activity: any) => (
+            <li key={activity.id}>
+              {activity.title}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
   );
-}
+  } 
+
+
 
 export default App;
--- client-app/src/app/layouts/NavBar.tsx
+++ client-app/src/app/layouts/NavBar.tsx
@@ -1,8 +1,22 @@
-import React from 'react';
-
-
-export default function NavBar() {}
-
- 
-
+import React from 'react'
+import { Button, Container, Menu } from 'semantic-ui-react'
     
+function NavBar() {
+    return (
+        <Menu inverted fixed='top'>
+            <Container>
+                <Menu.Item header>
+                    <img src="/images/death-star.png" alt="logo" style={{marginRight: '10px'}}/>
+                    FitnessStar
+                </Menu.Item>
+                <Menu.Item name='Activities'/> 
+                    
+                <Menu.Item>
+                    <Button positive content='Create Activity'/>
+                </Menu.Item>
+            </Container>
+        </Menu>
+    )
+}
+
+export default NavBar
